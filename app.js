c64466bf96896ac412078163e4415d38f06f2db7
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-02-24 17:21:50 +0100

    route

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 import { langArr } from './lang.js';
 
 const btnLangs = document.querySelectorAll('.btn-language');
-const allLang = ['pl', 'en', 'ua',];
+const allLang = ['pl', 'en', 'ua'];
 
 btnLangs.forEach(btnLang => {
-    btnLang.addEventListener('click', changeURLLanguage);
+    btnLang.addEventListener('click', changeLanguage);
 });
 
-function changeURLLanguage(event) {
+function changeLanguage(event) {
     let lang = event.target.value;
     console.log(lang);
-    location.href = window.location.pathname + '#' + lang;
-    location.reload();
+    updateTextContent(lang);
 }
 
-function changeLanguage(){
-    let hash = window.location.hash.substr(1);
-    if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + '#pl'
-        location.reload()
-    }  
-    btnLangs.forEach(btnLang => {
-        if (btnLang.value === hash) {
-            btnLang.checked = true;
-        }
-    });
-
-    document.querySelector('title').innerHTML = langArr['titleLang'][hash]
+function updateTextContent(lang) {
+    document.title = langArr['titleLang'][lang];
     for (let key in langArr) {
         let elems = document.querySelectorAll('.lng-' + key);
         elems.forEach(elem => {
-            elem.innerHTML = langArr[key][hash];
+            elem.innerHTML = langArr[key][lang];
         });
     }
 }
 
-changeLanguage()
+// Инициализация при загрузке страницы
+document.addEventListener("DOMContentLoaded", function(event) {
+    let lang = window.location.hash.substr(1);
+    if (!allLang.includes(lang)) {
+        lang = 'pl';
+    }
+    updateTextContent(lang);
+    btnLangs.forEach(btnLang => {
+        if (btnLang.value === lang) {
+            btnLang.checked = true;
+        }
+    });
+});

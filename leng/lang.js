96cd97196ec6b4a8259c446e329a408d9dcec1dc
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-02-29 22:07:31 +0100

    contacs

--- leng/lang.js
+++ leng/lang.js
@@ -29,4 +29,14 @@ export const langArr = {
         "en": "My name is Jakub Jagoda. I come from a small coastal town in Poland. For over three years I've been exploring the dark sides of mythology, folklore, and fairy tales through my digital paintings. My art has been exhibited in Paris, Rome, Seoul, Lisbon, and NYC. I've been working on many projects including but not limited to album covers, book illustrations, and card art. I love helping people bring their worlds to reality through my art.",
         "ua": "Мене звати Якуб Ягода. Я родом із маленького приморського містечка в Польщі. Більше трьох років я цікавлюсь темними сторонами міфології, фольклору та казок які показую через свої картини. Мої роботи виставлялися в Парижі, Римі, Сеулі, Лісабоні та Нью-Йорку. Я працював над багатьма проектами, включаючи обкладинки альбомів, книжкові ілюстрації та листівки, серед іншого. Мені подобається допомагати людям усвідомлювати їхній світ через своє мистецтво.",
     },
+    "h3__form":{
+        "pl": "Jak możemy ci pomóc?",
+        "en": "How can we help you?",
+        "ua": "Як ми можемо допомогти вам?",
+    },
+    "h3__contact":{
+        "pl": "Skontaktuje się z nami",
+        "en": "Contacts us",
+        "ua": "Звяжіться з нами",
+    },
 }
